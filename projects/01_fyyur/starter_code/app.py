453b77642bd76c1645eb0258283e13f5f67c60e6
raphaelDuff <raphaelprates@outlook.com>
2024-07-16 20:59:39 -0300

    [fyyur] New artist function
    - Creating artist functionality was added

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -34,6 +34,7 @@ from models import (
 import os
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import select, delete, func, literal
+import sys
 from typing import Optional
 
 # ----------------------------------------------------------------------------#
@@ -452,14 +453,42 @@ def create_artist_form():
 
 @app.route("/artists/create", methods=["POST"])
 def create_artist_submission():
-    # called upon submitting the new artist listing form
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
 
-    # on successful db insert, flash success
-    flash("Artist " + request.form["name"] + " was successfully listed!")
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+    form = ArtistForm()
+
+    if form.validate_on_submit():
+        try:
+            stmt_select_genres = select(Genre).where(Genre.name.in_(form.genres.data))
+            genres_from_form = db.session.scalars(stmt_select_genres).all()
+            max_id = db.session.query(func.max(Artist.id)).first()[0]
+            new_id = max_id + 1 if max_id else 1
+            artist = Artist(
+                id=new_id,
+                name=form.name.data,
+                city=form.city.data,
+                state=form.state.data,
+                phone=form.phone.data,
+                genres=genres_from_form,
+                facebook_link=form.facebook_link.data,
+                image_link=form.image_link.data,
+                website=form.website_link.data,
+                seeking_venue=form.seeking_venue.data,
+                seeking_description=form.seeking_description.data,
+            )
+
+            db.session.add(artist)
+            db.session.commit()
+            # on successful db insert, flash success
+            flash("Artist " + request.form["name"] + " was successfully listed!")
+        except:
+            flash(
+                "An error occurred. Artist " + form.name.data + " could not be created."
+            )
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
     return render_template("pages/home.html")
 
 
